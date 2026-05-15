b0a10d19d40ffbefa9ee12dfdf564674dae5913a
Jan <jansschule81@gmail.com>
2026-05-15 18:00:07 +0200

    Align command descriptions in help output

--- src/userland/cli/help.c
+++ src/userland/cli/help.c
@@ -37,10 +37,10 @@ int main(int argc, char **argv) {
     printf("clear          - Clear the screen\n");
     printf("exit           - Exit the terminal\n");
     printf("net            - Network tools\n");
-    printf("find       - find files or folders\n");
-    printf("rev        - Reverse a string or file\n");
-    printf("head       - print lines from the top down\n");
-    printf("tail       - print lines from the bottom up\n");
-    printf("time <cmd> - Measure command execution time\n");
+    printf("find           - find files or folders\n");
+    printf("rev            - Reverse a string or file\n");
+    printf("head           - print lines from the top down\n");
+    printf("tail           - print lines from the bottom up\n");
+    printf("time <cmd>     - Measure command execution time\n");
     printf("\nHint: Use Ctrl+C to force quit any running application.\n");
     return 0;
