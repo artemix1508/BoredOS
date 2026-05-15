94768017f106d158b756690e79a73368c2a97139
Jan <jansschule81@gmail.com>
2026-05-15 16:56:58 +0200

    Update copyright notice in rev.c file

--- src/userland/cli/rev.c
+++ src/userland/cli/rev.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2026 janevers (https://github.com/zeyadhost)
+// Copyright (c) 2026 janevers (https://github.com/janevers-sys)
 // This software is released under the GNU General Public License v3.0. See LICENSE file for details.
 // This header needs to maintain in any file it is present in, as per the GPL license terms.
 
